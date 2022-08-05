509914e3d2d1fec7dd46dfcd75a5a553ce121eb4
batuhan34 <bfindik@ekofactoring.com>
2022-08-05 13:08:05 +0300

    .

--- src/Components/ResetPassword.js
+++ src/Components/ResetPassword.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useLayoutEffect, useState, useRef} from "react";
 import './ResetPassword.css';
 import {connect} from 'react-redux';
 import { startloading, endloading } from '../action/loader';
@@ -13,8 +13,11 @@ const ResetPassword = (props) => {
 
   const [userMail, setUserMail] = useState("");
   const [validateEmail, setvalidateEmail] = useState(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-  
-
+  const [counter, setCounter] = useState();
+  const [disabledLink, setDisabledLink] = useState(false);
+  const [myInterval, setMyInterval] = useState(false);
+  const [intervalId, setIntervalId] = useState(0);
+  const firstUpdate = useRef(true);
 
   const inputControl = () => {
     if(userMail == "")
@@ -30,33 +33,72 @@ const ResetPassword = (props) => {
       return false;
   }
 
-  const resetPassword = () => {
-    if(inputControl())
-      swal("Mail Zorunludur!", "", "error");
-    else if(mailControl())
-      swal("Lütfen Geçerli Bir Mail Adresi Giriniz!", "", "error");
-    else{
-      props.dispatch(startloading())
-      post('User/ResetPassword',{Mail: userMail})
-      .then(resp=>{
-        if(resp === 200){
-          swal({title: "Şifrenizi Sıfırlamanız İçin Size Mail Gönderdik..",
-          text: "",
-          icon: "warning",
-          dangerMode: true,});
-          props.dispatch(endloading());
-
-        }else if(resp === 403){
-          swal("Kayıtlı Mail Adresi Bulunamadı!", "", "error");
-          props.dispatch(endloading()) 
-        }else{
-          swal(resp.data || "", "", "error");
-          props.dispatch(endloading())
-        }
-     })
+  const resetPassword = (e) => { 
+    if(!disabledLink){
+      if(inputControl())
+        swal("Mail Zorunludur!", "", "error");
+      else if(mailControl())
+        swal("Lütfen Geçerli Bir Mail Adresi Giriniz!", "", "error");
+      else{
+        props.dispatch(startloading())
+        post('User/ResetPassword',{Mail: userMail})
+        .then(resp=>{
+          if(resp === 200){
+            swal({title: "Şifrenizi Sıfırlamanız İçin Size Mail Gönderdik..",
+            text: "",
+            icon: "warning",
+            dangerMode: true,});
+            props.dispatch(endloading());
+            setDisabledLink(true);
+            e.target.classList.add('disabledLink');
+            setMyInterval(0);
+          }else if(resp === 403){
+            swal("Kayıtlı Mail Adresi Bulunamadı!", "", "error");
+            props.dispatch(endloading()) 
+          }else{
+            swal(resp.data || "", "", "error");
+            props.dispatch(endloading())
+          }
+        })
+      }
     }
   }
 
+  useLayoutEffect(()=>{
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    if(!myInterval) setIntervalId(startCounter())
+    else{
+      clearInterval(intervalId);  
+      document.getElementById('resetPassword').classList.remove('disabledLink');
+      setDisabledLink(false);
+    } 
+  },[myInterval])
+
+  const startCounter = () =>{
+    let dakika = 4
+    let saniye = 60;
+    return setInterval(() => {
+      if(saniye === "00"){
+        saniye = 60;
+        dakika = dakika - 1;
+      }
+      if(saniye < 11){
+          let veri = saniye-1;
+          saniye = 0+""+veri;
+      }else{
+          saniye = saniye-1;
+      }
+    
+      setCounter(dakika+":"+saniye)
+
+      if(dakika === 0 && saniye === "00"){
+        setMyInterval(true)
+      }
+    }, 1000);
+  }
 
   return(
     <>
@@ -68,7 +110,8 @@ const ResetPassword = (props) => {
             <label>Mail</label>
           </div>
           <div className="resetPasswordMain">
-            <Link onClick={resetPassword} to="#" className="resetPassword">Gönder</Link>          
+            <Link onClick={(e)=>resetPassword(e)} to="#" id="resetPassword" className="resetPassword">Gönder</Link>        
+            <span className="counter">{counter}</span>  
           </div>
         </form>
       </div>
